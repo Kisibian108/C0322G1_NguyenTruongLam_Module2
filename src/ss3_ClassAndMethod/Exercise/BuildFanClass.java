095edfb99f780c11f92da14398d80b6486c5769e
Kisibian108 <102302311+Kisibian108@users.noreply.github.com>
2022-05-10 15:27:27 +0700

    Demo

---
+++ src/ss3_ClassAndMethod/Exercise/BuildFanClass.java
@@ -0,0 +1,74 @@
+package ss3_ClassAndMethod.Exercise;
+
+public class BuildFanClass {
+    final int SLOW = 1, MEDIUM = 2, FAST = 3;
+    boolean on = false;
+    double radius;
+    String color;
+
+    public BuildFanClass() {
+    }
+
+    private int speed() {
+        return SLOW;
+    }
+
+    private boolean status() {
+        return false;
+    }
+
+    private double radius() {
+        return 5;
+    }
+
+    private String color() {
+        return "blue";
+    }
+
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    private boolean on() {
+        return true;
+    }
+
+    private boolean off() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "BuildFanClass{" + " Fan is On" +
+                ", FAST=" + FAST +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public String toString1() {
+        return "BuildFanClass{" + " Fan is Off" +
+                ", MEDIUM=" + MEDIUM +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+}
---
+++ src/ss3_ClassAndMethod/Exercise/Main2.java
@@ -0,0 +1,16 @@
+package ss3_ClassAndMethod.Exercise;
+
+public class Main2 {
+    public static void main(String[] args) {
+        BuildFanClass buildFanClass1 = new BuildFanClass();
+
+        buildFanClass1.setColor("Yellow");
+        buildFanClass1.setRadius(10);
+        System.out.println(buildFanClass1.toString());
+
+        BuildFanClass buildFanClass2 = new BuildFanClass();
+        buildFanClass2.setColor("Blue");
+        buildFanClass2.setRadius(5);
+        System.out.println(buildFanClass2.toString1());
+    }
+}
